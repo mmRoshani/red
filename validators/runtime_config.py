4e3025e63c1ff9c21add6f8bfc0a62f35467afda
khamideh <khamideh@outlook.com>
2025-06-09 14:53:17 +0330

    Refactor[app]: feature

--- validators/runtime_config.py
+++ validators/runtime_config.py
@@ -9,11 +9,9 @@ class RuntimeConfig:
 
     clients_id_list: List[str] | List[int]
 
-    # runtime datasets
     train_loaders: List[DataLoader]
     test_loaders: List[DataLoader]
 
     # TODO: HE related runtime configurations
-
-    # application Log instance
+    
     log: Log
